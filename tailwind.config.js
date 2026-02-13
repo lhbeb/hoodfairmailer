4a5322a27522989abdbc4859d72fc03eabb6a55f
shadowhacker8 <happyemilliano@gmail.com>
2026-02-13 02:09:18 +0100

    kjhhb

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        accent: {
+          DEFAULT: '#2356A5',
+          hover: '#1b4382',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
